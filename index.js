6dd5333493f2b78cb8ee57f97911a587674ca569
VijaySaiBorru <134841763+VijaySaiBorru@users.noreply.github.com>
2025-03-18 04:23:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 const corsOptions = {
-  origin: "http://localhost:5173", 
+  origin: ["http://localhost:5173","https://gc-frontend-git-main-vijay-sai-borrus-projects.vercel.app"], 
   credentials: true,             
   optionsSuccessStatus: 200,    
 };
